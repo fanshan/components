55b13951d0ce0584d6022950f6d32b3a09be19d5
Gauthier Delamarre <gauthier@objective-php.org>
2016-04-23 11:24:26 +0200

    Update code formatting

--- src/Invokable/Invokable.php
+++ src/Invokable/Invokable.php
@@ -71,18 +71,18 @@ class Invokable extends AbstractInvokable
         $operation = $this->operation;
         try
         {
-            if (!is_callable($operation))
+            if(!is_callable($operation))
             {
-                if ($operation instanceof ServiceReference)
+                if($operation instanceof ServiceReference)
                 {
                     $serviceId = $operation->getId();
 
-                    if (is_null($this->getServicesFactory()))
+                    if(is_null($this->getServicesFactory()))
                     {
                         throw new Exception(sprintf('No ServicesFactory is available to build referenced service "%s"', $serviceId));
                     }
 
-                    if (!$this->getServicesFactory()->has($operation))
+                    if(!$this->getServicesFactory()->has($operation))
                     {
                         throw new Exception(sprintf('Referenced service "%s" is not registered', $serviceId), Exception::REFERENCED_SERVICE_IS_NOT_REGISTERED);
                     }
@@ -90,20 +90,20 @@ class Invokable extends AbstractInvokable
                     try
                     {
                         $operation = $this->getServicesFactory()->get($operation);
-                    } catch (ServicesFactoryException $e)
+                    } catch(ServicesFactoryException $e)
                     {
                         throw new Exception(sprintf('An error occurred when building referenced service "%s"', $serviceId), Exception::REFERENCED_SERVICE_BUILD_ERROR, $e);
                     }
 
-                    if (!is_callable($operation))
+                    if(!is_callable($operation))
                     {
                         throw new Exception(sprintf('Referenced service "%s" is not an instance of a callable class ("%s" should implement __invoke())', $serviceId, get_class($operation)), Exception::REFERENCED_SERVICE_IS_NOT_CALLABLE);
                     }
-                } elseif (class_exists($operation))
+                } elseif(class_exists($operation))
                 {
                     $operation = new $operation;
 
-                    if (!is_callable($operation))
+                    if(!is_callable($operation))
                     {
                         throw new Exception(sprintf('Class "%s" is not callable (it should implement __invoke())', get_class($operation)), Exception::CLASS_IS_NOT_INVOKABLE);
                     }
@@ -113,7 +113,7 @@ class Invokable extends AbstractInvokable
                 }
             }
 
-        } catch (Exception $e)
+        } catch(Exception $e)
         {
             throw new Exception(sprintf('Cannot run operation: %s', $this->getDescription()), Exception::FAILED_RUNNING_OPERATION, $e);
         }
@@ -129,8 +129,9 @@ class Invokable extends AbstractInvokable
 
         $operation = $this->operation;
 
-        switch (true)
+        switch(true)
         {
+
             case $operation instanceof ServiceReference:
                 $description = 'Referenced service "' . $operation->getId() . '"';
                 break;
@@ -140,12 +141,26 @@ class Invokable extends AbstractInvokable
                 $description = sprintf('Closure defined in file "%s" on line %d', $reflected->getFileName(), $reflected->getStartLine());
                 break;
 
-            case is_object($operation):
-                $description = 'Instance of ' . get_class($operation);
+            case is_object($operation) && is_callable($operation):
+                $description = 'Instance of callable class ' . get_class($operation);
                 break;
 
             case is_string($operation) && class_exists($operation):
                 $description = 'Invokable class ' . $operation;
+                break;
+
+            case is_callable($operation):
+                if(is_array($operation))
+                {
+                    if(is_object($operation[0]))
+                    {
+                        $description = 'Dynamic call to ' . get_class($operation[0]) . '::' . $operation[1] . '()';
+                    }
+                    else
+                    {
+                        $description = 'Static call to ' . get_class($operation[0]) . '::' . $operation[1] . '()';
+                    }
+                } else $description = sprintf('Native callable');
                 break;
 
             default:
